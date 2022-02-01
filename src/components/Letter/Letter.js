240dbbc8c38ef3cfc58275ade7a0a6a7e3447af3
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-01 18:19:20 +0100

    Letter li added on return

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -1,5 +1,9 @@
-const Letter = () => {
-return ()  
-}
+const Letter = ({ letter, actionOnClick }) => {
+  return (
+    <li onClick={actionOnClick} className="letter">
+      <a href={letter}>{letter}</a>
+    </li>
+  );
+};
 
-export default Letter
+export default Letter;
