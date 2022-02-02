9129941a6d73ef8d5606e222149d3b1efdfe0f0d
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-02 01:18:18 +0100

    scss added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.scss";
+import "./scss/App.scss";
 import Letter from "./components/Letter/Letter";
 import GameResult from "./components/GameResult/GameResult";
 import UsedLetter from "./components/UsedLetter/UsedLetter";
@@ -69,19 +69,13 @@ function App() {
   return (
     <>
       <div className="hangman-container">
-        <svg className="hangman" viewBox="0 0 96 96" width="300" height="300">
+        <svg className="hangman" viewBox="0 0 96 96" width="200" height="200">
           <line className="stage11" x1="62" y1="70" x2="56" y2="56"></line>
           <line className="stage10" x1="50" y1="70" x2="56" y2="56"></line>
           <line className="stage9" x1="68" y1="46" x2="56" y2="40"></line>
           <line className="stage8" x1="44" y1="46" x2="56" y2="40"></line>
           <line className="stage7" x1="56" y1="40" x2="56" y2="56"></line>
-          <circle
-            className="stage6"
-            cx="56"
-            cy="34"
-            r="6"
-            fill="#bee5eb"
-          ></circle>
+          <circle className="stage6" cx="56" cy="34" r="6" fill="#fff"></circle>
           <line className="stage5" x1="56" y1="16" x2="56" y2="28"></line>
           <line className="stage4" x1="24" y1="24" x2="32" y2="16"></line>
           <line className="stage3" x1="21" y1="16" x2="60" y2="16"></line>
