17be45cae88dc8ff348e6e20154af8e982f045af
AdrianValladares <a.valladares.d@gmail.com>
2022-02-02 08:53:48 +0100

    added hanged man

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./scss/App.scss";
 import Letter from "./components/Letter/Letter";
 import GameResult from "./components/GameResult/GameResult";
 import UsedLetter from "./components/UsedLetter/UsedLetter";
-
 import GuessedLetters from "./components/GuessedLetters/GuessedLetters";
+import Hangman from "./components/Hangman/Hangman";
 
 /* import words from "./data/words"; */
 
@@ -31,7 +31,7 @@ function App() {
   const [gameState, setGameState] = useState(undefined);
 
   const checkIfLost = () => {
-    if (numberOfErrors > 5) {
+    if (numberOfErrors > 10) {
       setGameState("lost");
     }
   };
@@ -69,19 +69,7 @@ function App() {
   return (
     <>
       <div className="hangman-container">
-        <svg className="hangman" viewBox="0 0 96 96" width="200" height="200">
-          <line className="stage11" x1="62" y1="70" x2="56" y2="56"></line>
-          <line className="stage10" x1="50" y1="70" x2="56" y2="56"></line>
-          <line className="stage9" x1="68" y1="46" x2="56" y2="40"></line>
-          <line className="stage8" x1="44" y1="46" x2="56" y2="40"></line>
-          <line className="stage7" x1="56" y1="40" x2="56" y2="56"></line>
-          <circle className="stage6" cx="56" cy="34" r="6" fill="#fff"></circle>
-          <line className="stage5" x1="56" y1="16" x2="56" y2="28"></line>
-          <line className="stage4" x1="24" y1="24" x2="32" y2="16"></line>
-          <line className="stage3" x1="21" y1="16" x2="60" y2="16"></line>
-          <line className="stage2" x1="24" y1="80" x2="24" y2="16"></line>
-          <line className="stage1" x1="16" y1="80" x2="32" y2="80"></line>
-        </svg>
+        <Hangman nOfErrors={numberOfErrors} />
       </div>
       <ul className="guess-letters">
         {solutionLetters.map((letter) => {
